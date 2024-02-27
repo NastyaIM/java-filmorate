511f5a0693e19fc6cfc5623196711a307fadda1d
Nastya <nimalyshevann@gmail.com>
2024-02-27 18:42:09 +0300

    fix: убрать throws

--- src/main/java/ru/yandex/practicum/filmorate/model/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ValidationException.java
@@ -1,7 +1,5 @@
 package ru.yandex.practicum.filmorate.model;
 
-import org.apache.tomcat.util.digester.RulesBase;
-
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
